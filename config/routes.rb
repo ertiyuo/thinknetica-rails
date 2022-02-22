d6a235ead3766426672dd5c7962add27d97c8a61
Anastasiia Grishchenko <ertiyuo@x01d.com>
2022-02-22 19:39:07 +0500

    feat: create about author page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/about', to: "about#index"
+  get '/about/author', to: "about#author"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
